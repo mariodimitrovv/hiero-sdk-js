b206e01a88a90916a3c9085362bce25126e3efb8
ivaylonikolov7 <ivaylo.nikolov@limechain.tech>
2025-02-28 11:21:40 +0200

    fix: build issues with browser aliases (#2902)
    * fix: build issues with browser aliases
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    * build: update package.json's import property
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    * chore: bump package.json and pnpm-lock.yaml
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    * chore: remove pnpm version
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    * build: update aliases
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>
    
    ---------
    
    Signed-off-by: Ivaylo Nikolov <ivaylo.nikolov@limechain.tech>

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,114 @@
 import terser from "@rollup/plugin-terser";
+import alias from "@rollup/plugin-alias";
+
+const browserAliases = {
+    entries: [
+        {
+            find: "../src/encoding/hex.js",
+            replacement: "../src/encoding/hex.browser.js",
+        },
+        {
+            find: "../../src/encoding/hex.js",
+            replacement: "../../src/encoding/hex.browser.js",
+        },
+        {
+            find: "../../../src/encoding/hex.js",
+            replacement: "../../../src/encoding/hex.browser.js",
+        },
+        {
+            find: "src/encoding/hex.js",
+            replacement: "src/encoding/hex.browser.js",
+        },
+        {
+            find: "../encoding/hex.js",
+            replacement: "../encoding/hex.browser.js",
+        },
+        { find: "./encoding/hex.js", replacement: "./encoding/hex.browser.js" },
+        {
+            find: "../src/encoding/utf8.js",
+            replacement: "../src/encoding/utf8.browser.js",
+        },
+        {
+            find: "../../src/encoding/utf8.js",
+            replacement: "../../src/encoding/utf8.browser.js",
+        },
+        {
+            find: "../encoding/utf8.js",
+            replacement: "../encoding/utf8.browser.js",
+        },
+        {
+            find: "../src/cryptography/sha384.js",
+            replacement: "../src/cryptography/sha384.browser.js",
+        },
+        {
+            find: "../cryptography/sha384.js",
+            replacement: "../cryptography/sha384.browser.js",
+        },
+        {
+            find: "./client/NodeIntegrationTestEnv.js",
+            replacement: "./client/WebIntegrationTestEnv.js",
+        },
+        {
+            find: "../integration/client/NodeIntegrationTestEnv.js",
+            replacement: "../integration/client/WebIntegrationTestEnv.js",
+        },
+        {
+            find: "../../src/client/NodeClient.js",
+            replacement: "../../src/client/WebClient.js",
+        },
+        // Add the index.js redirects
+        { find: "../../src/index.js", replacement: "../../src/browser.js" },
+        { find: "../src/index.js", replacement: "../src/browser.js" },
+    ],
+};
+
+const nativeAliases = {
+    entries: [
+        { find: "../src/index.js", replacement: "../src/native.js" },
+        {
+            find: "../src/encoding/hex.js",
+            replacement: "../src/encoding/hex.native.js",
+        },
+        {
+            find: "../src/encoding/utf8.js",
+            replacement: "../src/encoding/utf8.native.js",
+        },
+        {
+            find: "../src/cryptography/sha384.js",
+            replacement: "../src/cryptography/sha384.native.js",
+        },
+    ],
+};
 
 export default [
     {
-        input: "src/index.js",
+        input: "src/browser.js",
+        plugins: [alias(browserAliases), terser()],
         output: {
             dir: "lib/",
             format: "esm",
             sourcemap: true,
             preserveModules: true,
-            plugins: [terser()],
+        },
+    },
+    {
+        input: "src/native.js",
+        plugins: [terser(), alias(nativeAliases)],
+        output: {
+            dir: "lib/",
+            format: "esm",
+            sourcemap: true,
+            preserveModules: true,
+        },
+    },
+    {
+        input: "src/index.js",
+        plugins: [terser()],
+        output: {
+            dir: "lib/",
+            format: "esm",
+            sourcemap: true,
+            preserveModules: true,
         },
     },
 ];
