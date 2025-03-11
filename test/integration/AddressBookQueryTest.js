b4921bff2b288a52486d072f7c68e1991b49ce46
ivaylogarnev <ivaylo.garnev@limechain.tech>
2025-03-11 14:36:34 +0200

    feat: Added `AddressBookQuery` test against Local node/Solo (#2933)
    * tests: Added local node addressbook test, confirmed it works with solo also
    
    Signed-off-by: ivaylogarnev-limechain <ivaylo.garnev@limechain.tech>
    
    * docs: Added comment
    
    Signed-off-by: ivaylogarnev-limechain <ivaylo.garnev@limechain.tech>
    
    ---------
    
    Signed-off-by: ivaylogarnev-limechain <ivaylo.garnev@limechain.tech>

--- test/integration/AddressBookQueryTest.js
+++ test/integration/AddressBookQueryTest.js
@@ -1,7 +1,20 @@
 import { AddressBookQuery } from "../../src/exports.js";
 import { Client } from "./client/NodeIntegrationTestEnv.js";
+import IntegrationTestEnv from "./client/NodeIntegrationTestEnv.js";
 
 describe("AddressBookQuery", function () {
+    it("should be query the addressbook on local node", async function () {
+        // NOTE: Ensure the environment is configured for local use
+        const env = await IntegrationTestEnv.new();
+
+        const addressBook = await new AddressBookQuery()
+            .setFileId("0.0.102")
+            .execute(env.client);
+
+        expect(addressBook.nodeAddresses.length).to.be.above(0);
+        env.client.close();
+    });
+
     it("should be query the addressbook on testnet", async function () {
         const client = Client.forTestnet();
 
