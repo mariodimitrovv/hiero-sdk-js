a57f8ffe9238c5ef738e528a965173114f750f38
Ivan Ivanov <ivanivanov.ii726@gmail.com>
2025-07-11 16:23:14 +0300

    feat(TCK): Added FileUpdateTransaction endpoint (#3226)
    Signed-off-by: ivaylogarnev-limechain <ivaylo.garnev@limechain.tech>
    Signed-off-by: Ivan Ivanov <ivanivanov.ii726@gmail.com>
    Co-authored-by: ivaylogarnev-limechain <ivaylo.garnev@limechain.tech>

--- tck/methods/file.ts
+++ tck/methods/file.ts
@@ -1,4 +1,8 @@
-import { FileCreateTransaction, Timestamp } from "@hashgraph/sdk";
+import {
+    FileCreateTransaction,
+    FileUpdateTransaction,
+    Timestamp,
+} from "@hashgraph/sdk";
 import Long from "long";
 
 import { applyCommonTransactionParams } from "../params/common-tx-params";
@@ -52,6 +56,56 @@ export const createFile = async ({
 
     return {
         fileId: receipt.fileId.toString(),
+        status: receipt.status.toString(),
+    };
+};
+
+export const updateFile = async ({
+    fileId,
+    keys,
+    contents,
+    expirationTime,
+    memo,
+    commonTransactionParams,
+}: any): Promise<FileResponse> => {
+    const transaction = new FileUpdateTransaction().setGrpcDeadline(
+        DEFAULT_GRPC_DEADLINE,
+    );
+
+    if (fileId != null) {
+        transaction.setFileId(fileId);
+    }
+
+    if (keys != null) {
+        transaction.setKeys(keys.map((key: string) => getKeyFromString(key)));
+    }
+
+    if (contents != null) {
+        transaction.setContents(contents);
+    }
+
+    if (expirationTime != null) {
+        transaction.setExpirationTime(
+            new Timestamp(Long.fromString(expirationTime), 0),
+        );
+    }
+
+    if (memo != null) {
+        transaction.setFileMemo(memo);
+    }
+
+    if (commonTransactionParams != null) {
+        applyCommonTransactionParams(
+            commonTransactionParams,
+            transaction,
+            sdk.getClient(),
+        );
+    }
+
+    const response = await transaction.execute(sdk.getClient());
+    const receipt = await response.getReceipt(sdk.getClient());
+
+    return {
         status: receipt.status.toString(),
     };
 };
--- tck/params/file.ts
+++ tck/params/file.ts
@@ -5,3 +5,12 @@ export interface FileCreateParams {
     readonly memo: string;
     readonly commonTransactionParams?: Record<string, any>;
 }
+
+export interface FileUpdateParams {
+    readonly fileId: string;
+    readonly keys: string[];
+    readonly contents: string;
+    readonly expirationTime: string;
+    readonly memo: string;
+    readonly commonTransactionParams: Record<string, any>;
+}
